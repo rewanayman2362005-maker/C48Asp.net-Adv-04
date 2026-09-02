33e83306fa16e8cc899011add727c400c2073d63
rewanayman2362005 <rewanayman2362005@gmail.com>
2026-09-02 13:50:09 +0300

    EX5 Q2

--- stack&& queue route/Program.cs
+++ stack&& queue route/Program.cs
@@ -19,8 +19,16 @@ namespace stack___queue_route
             //}
             //Console.WriteLine("Count: {0}", documents.Count);
             #endregion
-
-
+            #region 2. Use Peek to see which document will print next (without removing)
+            //Queue<string> documents = new Queue<string>();
+            //documents.Enqueue("Report.pdf");
+            //documents.Enqueue("Invoice.pdf");
+            //documents.Enqueue("Letter.docx");
+            //documents.Enqueue("Resume.pdf");
+            //documents.Enqueue("Photo.jpg");
+ 
+            //Console.WriteLine("Next document to print: {0}", documents.Peek());
+            #endregion
         }
     }
 }
