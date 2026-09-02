995c2b2f564a0a9221aa0a06d4a2ec1ff5bbef61
rewanayman2362005 <rewanayman2362005@gmail.com>
2026-09-02 13:39:44 +0300

    Add project files.

---
+++ stack&& queue route/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Diagnostics.Metrics;
+
+namespace stack___queue_route
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+        }
+    }
+}
