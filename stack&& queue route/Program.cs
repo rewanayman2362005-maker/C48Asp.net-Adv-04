2888f55b8c2c90fe2b2d4e7a49ad935b677af68d
rewanayman2362005 <rewanayman2362005@gmail.com>
2026-09-02 13:41:38 +0300

    EX5 Q1

--- stack&& queue route/Program.cs
+++ stack&& queue route/Program.cs
@@ -6,6 +6,19 @@ namespace stack___queue_route
     {
         static void Main(string[] args)
         {
+            #region 1. Print the queue contents and Count 
+            //Queue<string> documents = new Queue<string>();
+            //documents.Enqueue("Report.pdf");
+            //documents.Enqueue("Invoice.pdf");
+            //documents.Enqueue("Letter.docx");
+            //documents.Enqueue("Resume.pdf");
+            //documents.Enqueue( "Photo.jpg");
+            //foreach (var document in documents)
+            //{
+            //    Console.WriteLine(document);
+            //}
+            //Console.WriteLine("Count: {0}", documents.Count);
+            #endregion
 
 
         }
